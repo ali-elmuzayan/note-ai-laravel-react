769511ebec38ded729d4819abbc9c4a622d4760a
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-04-13 13:42:50 +0200

    add libraries: react router, shadcn UI & tailwind. Then implement the base structure of our application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import AppLayout from "./layouts/AppLayout";
+import AuthGuard from "./components/AuthGuard";
+import AuthLayout from "./layouts/AuthLayout";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Auth Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+        </Route>
+
+        {/* The Application With Protected routes  */}
+        <Route element={<AuthGuard />}>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<div>Home</div>} />
+          </Route>
+        </Route>
+
+        {/* Not Found Route */}
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
