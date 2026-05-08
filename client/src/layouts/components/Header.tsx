a37fc69f110a6e70c8ddaa957c3bad79d7cbdbf4
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-05-08 22:28:00 +0300

    implement some change in the global style (index.css) add the home page & note page, add the GlassCard component, and edit the header

--- client/src/layouts/components/Header.tsx
+++ client/src/layouts/components/Header.tsx
@@ -1,12 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <header className="w-full h-16 bg-white shadow-md flex items-center justify-between px-4">
-      <h1 className="text-3xl font-bold">Note AI</h1>
-      <Button variant="default" size="lg" onClick={() => alert("Logout")}>
-        Logout
-      </Button>
+    <header className="w-full sticky top-0 z-50">
+      <div className="max-w-6xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+        {/* glass card */}
+        <div className="glass-card h-16 shadow-md flex-between px-4">
+          {/* Logo  */}
+          <div className="flex items-center gap-3">
+            <Link
+              to="/"
+              className="text-sm font-semibold tracking-wide"
+              role="button"
+            >
+              PlanMe
+            </Link>
+            <p className="hidden text-xs text-muted-foreground sm:inline">
+              Note AI
+            </p>
+          </div>
+
+          <Button variant="default" size="lg" onClick={() => alert("Logout")}>
+            Logout
+          </Button>
+        </div>
+      </div>
     </header>
   );
 };
